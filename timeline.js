e4aa91ff58c9576284291b894259eb08617ce3b9
Jukka Eerikäinen <jukka.eerikainen@aalto.fi>
2017-04-19 13:31:15 +0300

    run simulation dynamically

--- timeline.js
+++ timeline.js
@@ -6,6 +6,7 @@ var height = +svg.attr("height") - margin.top - margin.bottom;
 var color = d3.scaleOrdinal(d3.schemeCategory10);
 var radius = 8;
 var minDist = radius + 1;
+var trackDist = 50;
 
 var formatValue = d3.format("d");
 
@@ -18,42 +19,35 @@ var g = svg.append("g")
 d3.json("mock-data.json", function(data) {
 	x.domain(d3.extent(data, function(d) { return d.start; }));
 	
-  var simulation = d3.forceSimulation(data)
-    .force("x", d3.forceX(function(d) { return x(d.start); }).strength(1))
-    .force("y", d3.forceY(height / 2))
-    .force("collide", d3.forceCollide(minDist))
-    .stop();
-
-  for (var i = 0; i < 200; ++i) simulation.tick();
-
+  // X-axis
   g.append("g")
-      .attr("class", "axis axis--x")
-      .attr("transform", "translate(0," + height + ")")
+    .attr("class", "axis axis--x")
+    .attr("transform", "translate(0," + height + ")")
       .call(d3.axisBottom(x).ticks(20, ""));
 
   var cell = g.append("g")
-      .attr("class", "cells")
-    .selectAll("g").data(d3.voronoi()
-        .extent([[-margin.left, -margin.top], [width + margin.right, height + margin.top]])
-        .x(function(d) { return d.x; })
-        .y(function(d) { return d.y; })
-      .polygons(data)).enter().append("g")
-      .on("click", function(){
-        //TODO expand
-        alert("You clicked on a circle, you!");
-      });
-
-  cell.append("circle")
+    .attr("class", "nodes")
+    .on("click", function() {alert("TODO");})
+    .selectAll("circle")
+    .data(data)
+    .enter().append("circle")
       .attr("r", radius)
-      .attr("fill", function(d) { return color(d.data.group); })
-      .attr("cx", function(d) { return d.data.x; })
-      .attr("cy", function(d) { return d.data.y; });
+      .attr("fill", function(d) { return color(d.group); })
 
-  cell.append("path")
-      .attr("d", function(d) { return "M" + d.join("L") + "Z"; });
+  cell
+    .append("title")
+      .text(function(d) { return d.content + "\n" + formatValue(d.start); });
 
-  cell.append("title")
-      .text(function(d) { return d.data.content + "\n" + formatValue(d.data.start); });
+  var simulation = d3.forceSimulation(data)
+    .force("x", d3.forceX(function(d) { return x(d.start); }).strength(0.1))
+    .force("y", d3.forceY(function(d) { return d.group * trackDist; }))
+    .force("collide", d3.forceCollide(minDist));
+
+  
+  simulation.on("tick", function() {
+    cell.attr("cx", function(d) { return d.x; }); 
+  });
+
 });
 
 function type(d) {
